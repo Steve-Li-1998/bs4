d58d160c4c4473db91f0f934e6270dd238ec41df
Yuyang Peng <44298389+Crayonst@users.noreply.github.com>
2021-06-19 20:06:48 +0200

    Update bestfit.c

--- bestfit.c
+++ bestfit.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
 #include "bestfit.h"
+
 
 /* Im Header sind MEM_POOL_SIZE und CHUNK_SIZE definiert*/
 
 /* der Speicherpool */
 static char mem_pool[MEM_POOL_SIZE];
+
 /* die Bitliste, die anzeigt, welche Bloecke frei sind: 1 Bit fuer jew. CHUNK_SIZE Bytes */
 static unsigned char free_list[MEM_POOL_SIZE / CHUNK_SIZE / 8];
 
@@ -15,6 +16,9 @@ static unsigned char free_list[MEM_POOL_SIZE / CHUNK_SIZE / 8];
 #define FREELIST_BIT_COUNT (sizeof(free_list) * 8)
 
 /* ----------------------------- Hilfsfunktionen ----------------------------- */
+
+
+
 
 /* Funktion zum Debuggen, die die Freispeicher-Bitliste ausgibt */
 #ifdef DEBUG
@@ -36,6 +40,9 @@ dump_free_mem(void)
 #  define dump_free_mem()
 #endif
 
+
+
+
 /* rechnet aus, wie viele Chunks fuer 'size' Bytes belegt werden muessen
    (rundet automatisch auf) */
 size_t
@@ -48,6 +55,9 @@ size_to_chunks(size_t size)
 	}
 }
 
+
+
+
 /* testet, ob im Bitfeld bitfield das Bit chunknr gesetzt ist:
  * liefert 0, wenn es nicht gesetzt ist, sonst eine Zahl > 0 */
 int
@@ -59,6 +69,7 @@ bit_is_set(const unsigned char *bitfield, unsigned chunknr)
 	return (bitfield[arrayPosition] & bitMask);
 }
 
+
 /* setzt im Bitfeld bitfield das Bit mit der Nummer chunknr */
 void
 set_bit(unsigned char *bitfield, unsigned chunknr)
@@ -68,6 +79,7 @@ set_bit(unsigned char *bitfield, unsigned chunknr)
 	int arrayPosition = chunknr/8;	
 	bitfield[arrayPosition] |= bitMask;
 }
+
 
 /* loescht im Bitfeld bitfield das Bit mit der Nummer chunknr */
 void
@@ -80,16 +92,56 @@ clear_bit(unsigned char *bitfield, unsigned chunknr)
 }
 
 
+
+
+
+
+
+
 /* ----------------------------- Speicherverwaltung ----------------------------- */
 
 void *
 bf_alloc(size_t size)
 {
-	/* HIER MUESST IHR EUREN CODE EINFUEGEN! */
+	if(size == 0){
+		return NULL;
+	}
+	
+	size_t chunk_index = 0;
+	size_t chunks_needed = size_to_chunks(size);
+	
+	while(chunk_index < CHUNK_SIZE){
+		if(free_list[chunk_index].status==0 && size_to_chunks(chunk_index)>=chunks_needed){
+            break;
+		}
+		chunk_index++;
+	}
+	if(chunk_index >= CHUNK_SIZE){
+        return NULL;
+	}
 
-	/* DENKT AUCH DARAN, DAS "return NULL;" GEEIGNET ANZUPASSEN! */
+	size_t reference_chunk_index = chunk_index;
 
-	return NULL;
+    while (reference_chunk_index < CHUNK_SIZE){
+        if(free_list[reference_chunk_index].status==0
+        && size_to_chunks(reference_chunk_index)>=chunks_needed
+        && size_to_chunks(reference_chunk_index)< size_to_chunks(chunk_index))
+            chunk_index = reference_chunk_index;
+            reference_chunk_index ++;
+    }
+
+    //size_t oldLength = free_list[chunk_index].length;
+    size_t oldLength = size_to_chunks(chunk_index);
+
+    free_list[chunk_index].status = ;
+    size_to_chunks(chunk_index) = chunks_needed;
+
+    if(oldLength > chunks_needed){
+        free_list[chunk_index + chunks_needed].status = 0;
+        size_to_chunks(chunk_index + chunks_needed) = oldLength - chunks_needed;
+        return chunk_index * CHUNK_SIZE;
+    }
+	
 }
 
 void
