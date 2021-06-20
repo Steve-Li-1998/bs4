2b4c305b412f61330a8cc9a0fae353ccbbd86d4f
Yuyang Peng <44298389+Crayonst@users.noreply.github.com>
2021-06-20 20:11:09 +0200

    Update bestfit.c

--- bestfit.c
+++ bestfit.c
@@ -15,11 +15,10 @@ static unsigned char free_list[MEM_POOL_SIZE / CHUNK_SIZE / 8];
 /* Anzahl der Bits in free_list */
 #define FREELIST_BIT_COUNT (sizeof(free_list) * 8)
 
+
+
+
 /* ----------------------------- Hilfsfunktionen ----------------------------- */
-
-
-
-
 /* Funktion zum Debuggen, die die Freispeicher-Bitliste ausgibt */
 #ifdef DEBUG
 void
@@ -103,43 +102,52 @@ clear_bit(unsigned char *bitfield, unsigned chunknr)
 void *
 bf_alloc(size_t size)
 {
+    //wenn size gleich 0, return NULL
 	if(size == 0){
 		return NULL;
 	}
-	
-	size_t chunk_index = 0;
-	size_t chunks_needed = size_to_chunks(size);
-	
-	while(chunk_index < CHUNK_SIZE){
-		if(free_list[chunk_index].status==0 && size_to_chunks(chunk_index)>=chunks_needed){
+
+	size_t chunkIndex = 0;     //fangt am die erste chunk an mit index 0
+	size_t needChunks = size_to_chunks(size);
+
+
+	while(chunkIndex < CHUNK_SIZE){
+
+		//wenn Position chunkIndex noch nicht besitzt wurde &&
+        if(bit_is_set(mem_pool, free_list[chunkIndex])==0 && size_to_chunks(chunkIndex)>=needChunks){
             break;
 		}
-		chunk_index++;
+		chunkIndex++;
 	}
-	if(chunk_index >= CHUNK_SIZE){
+
+
+    //wenn am Ende keine ausreichend große Luecke vorhanden ist, return NULL
+    if(chunkIndex >= CHUNK_SIZE){
         return NULL;
 	}
 
-	size_t reference_chunk_index = chunk_index;
 
-    while (reference_chunk_index < CHUNK_SIZE){
-        if(free_list[reference_chunk_index].status==0
-        && size_to_chunks(reference_chunk_index)>=chunks_needed
-        && size_to_chunks(reference_chunk_index)< size_to_chunks(chunk_index))
-            chunk_index = reference_chunk_index;
-            reference_chunk_index ++;
+	size_t tempIndex = chunkIndex;
+
+    while (tempIndex < CHUNK_SIZE){
+        if(bit_is_set(mem_pool[MEM_POOL_SIZE], free_list[chunkIndex])==0)
+        && size_to_chunks(tempIndex) >= needChunks
+        && size_to_chunks(tempIndex) < size_to_chunks(chunkIndex))
+
+            chunkIndex = tempIndex;
+            tempIndex ++;
     }
 
-    //size_t oldLength = free_list[chunk_index].length;
-    size_t oldLength = size_to_chunks(chunk_index);
+    //size_t oldLength = free_list[chunkIndex].length;
+    size_t oldLength = size_to_chunks(chunkIndex);
 
-    free_list[chunk_index].status = ;
-    size_to_chunks(chunk_index) = chunks_needed;
+    //free_list[chunkIndex].status = ;
+    size_to_chunks(chunkIndex) = needChunks;
 
-    if(oldLength > chunks_needed){
-        free_list[chunk_index + chunks_needed].status = 0;
-        size_to_chunks(chunk_index + chunks_needed) = oldLength - chunks_needed;
-        return chunk_index * CHUNK_SIZE;
+    if(oldLength > needChunks){
+        //free_list[chunkIndex + needChunks].status = 0;
+        size_to_chunks(chunkIndex + needChunks) = oldLength - needChunks;
+        return chunkIndex * CHUNK_SIZE;
     }
 	
 }
